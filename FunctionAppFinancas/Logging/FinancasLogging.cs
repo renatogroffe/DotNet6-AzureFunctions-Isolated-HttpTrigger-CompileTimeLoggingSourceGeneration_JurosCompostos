14a20b6da9de269b9cf1a3c84f40114919ac4096
Renato Groffe <renato.groffe@yahoo.com.br>
2022-01-13 18:49:11 -0300

    .NET 6 + Azure Functions + Isolated Process + Testes em Postman + HTTP Trigger + Compile-time logging source generation + Swagger/Open API
    .NET 6 + Azure Functions + Isolated Process + Testes em Postman + HTTP Trigger + Compile-time logging source generation + Swagger/Open API

---
+++ FunctionAppFinancas/Logging/FinancasLogging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppFinancas.Logging;
+
+public static partial class FinancasLogging
+{
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information,
+        Message = "Recebida nova requisição|Valor do empréstimo: {valorEmprestimo}|Número de meses: {numMeses}|% Taxa de Juros: {percTaxa}")]
+    public static partial void LogNovaRequisicao(
+        this ILogger logger, double? valorEmprestimo, int? numMeses, double? percTaxa);
+
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information,
+        Message = "Valor Final com Juros: {valorFinalJuros}")]
+    public static partial void LogValorJuros(
+        this ILogger logger, double? valorFinalJuros);
+}
